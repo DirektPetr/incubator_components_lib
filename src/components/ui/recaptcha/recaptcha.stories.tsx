e94d513a15a5d1b55c251607c34241a0a1b0c27b
DirektPetr <direktpetr@ya.ru>
2024-08-27 15:04:37 +0300

    fix recaptcha

--- src/components/ui/recaptcha/recaptcha.stories.tsx
+++ src/components/ui/recaptcha/recaptcha.stories.tsx
@@ -1,10 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Recaptcha } from './recaptcha'
+import Recaptcha from './recaptcha'
 
 const meta = {
   component: Recaptcha,
   title: 'Components/Recaptcha',
-  tags: ['autodocs'],
 } satisfies Meta<typeof Recaptcha>
 
 export default meta
@@ -13,14 +12,32 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    variant: 'default',
+    label: 'I’m not a robot',
+  },
+}
+
+export const Checked: Story = {
+  args: {
+    label: 'I’m not a robot',
+  },
+}
+
+export const Loading: Story = {
+  args: {
     label: 'I’m not a robot',
   },
 }
 
 export const Error: Story = {
   args: {
-    variant: 'error',
     label: 'I’m not a robot',
+    errorMsg: 'Please verify that you are not a robot',
+  },
+}
+
+export const Expired: Story = {
+  args: {
+    label: 'I’m not a robot',
+    expiredMsg: 'Verifiction expired. Check the checkbox again.',
   },
 }
--- src/components/ui/recaptcha/recaptcha.tsx
+++ src/components/ui/recaptcha/recaptcha.tsx
@@ -1,27 +1,64 @@
 import { ComponentPropsWithoutRef } from 'react'
 import s from './recaptcha.module.scss'
-import { Recaptchalogo1 } from '@/assets/components'
+import { CheckmarkOutline, Recaptchalogo1 } from '@/assets/components'
 import { clsx } from 'clsx'
 
 export type RecaptchaProps = {
   errorMsg?: string
-  variant?: 'default' | 'error' | 'checked' | 'loading' | 'expired'
+  expiredMsg?: string
+  type?: 'default' | 'checked' | 'loading'
   label?: string
+  isChecked?: boolean
 } & ComponentPropsWithoutRef<'div'>
 
-export function Recaptcha({ className, variant = 'default', ...rest }: RecaptchaProps) {
+const Recaptcha = ({
+  className,
+  label,
+  type = 'default',
+  isChecked,
+  expiredMsg,
+  errorMsg,
+  ...rest
+}: RecaptchaProps) => {
   return (
-    <div {...rest} className={clsx(s.recaptchaRoot, s[variant], className)}>
-      <div className={s.containerLeft}>
-        <input className={s.checkbox} type="checkbox" name="checkbox" id="checkbox" />
-        <label className={s.label}></label>
+    <div {...rest} className={errorMsg && s.error}>
+      <div className={clsx(s.recaptchaRoot, className)}>
+        {expiredMsg && <div className={expiredMsg ? s.expiredMsg : ''}>{expiredMsg}</div>}
+        <div className={clsx(s[type], className)}>
+          <label className={s.containerLeft}>
+            {type === 'default' && (
+              <input
+                className={s.checkbox}
+                type="checkbox"
+                name="checkbox"
+                id="checkbox"
+                checked={isChecked}
+              />
+            )}
+            {type === 'checked' && (
+              <div className={s.checkedIcon}>
+                <CheckmarkOutline />
+              </div>
+            )}
+            {type === 'loading' && (
+              <div className={s.loadingIcon}>
+                <span className={s.loader}></span>
+              </div>
+            )}
+            <span className={s.label}>{label}</span>
+          </label>
+
+          <div className={s.containerRight}>
+            <Recaptchalogo1 className={s.logo} />
+            <p className={s.privacy}>
+              <a href="#">Privacy</a> - Terms
+            </p>
+          </div>
+        </div>
       </div>
-      <div className={s.containerRight}>
-        <Recaptchalogo1 className={s.logo} />
-        <p className={s.privacy}>
-          <a href="#">Privacy</a> - Terms
-        </p>
-      </div>
+      <div className={errorMsg ? s.errorMsg : ''}>{errorMsg}</div>
     </div>
   )
 }
+
+export default Recaptcha
